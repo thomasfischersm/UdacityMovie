f4773a74adbaadf5b752022ea76061a9e782a810
thomasfischersm <thomas@playposse.com>
2017-08-18 18:50:28 -0700

    Got hello world to access the movie db working.

--- app/src/main/java/com/playposse/udacitymovie/DiscoverMoviesActivity.java
+++ app/src/main/java/com/playposse/udacitymovie/DiscoverMoviesActivity.java
@@ -1,13 +1,35 @@
 package com.playposse.udacitymovie;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import info.movito.themoviedbapi.TmdbApi;
+import info.movito.themoviedbapi.model.Discover;
+import info.movito.themoviedbapi.model.MovieDb;
+import info.movito.themoviedbapi.model.core.MovieResultsPage;
 
 public class DiscoverMoviesActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = DiscoverMoviesActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover_movies);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                TmdbApi tmdbApi = new TmdbApi(Secrets.getMovieApiKeyV3());
+                Discover discover = new Discover();
+                discover.sortBy("popularity.desc");
+                MovieResultsPage resultsPage = tmdbApi.getDiscover().getDiscover(discover);
+
+                for (MovieDb movie : resultsPage.getResults() ) {
+                    Log.i(LOG_TAG, "onCreate: Movie is: " + movie.getTitle());
+                }
+            }
+        }).start();
     }
 }
