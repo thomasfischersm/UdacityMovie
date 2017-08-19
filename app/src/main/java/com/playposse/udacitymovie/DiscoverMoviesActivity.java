4eb2e5164f259d6dc53a6fa74cb5eacacc12e01d
thomasfischersm <thomas@playposse.com>
2017-08-18 18:28:57 -0700

    Initial commit

---
+++ app/src/main/java/com/playposse/udacitymovie/DiscoverMoviesActivity.java
@@ -0,0 +1,13 @@
+package com.playposse.udacitymovie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DiscoverMoviesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discover_movies);
+    }
+}
